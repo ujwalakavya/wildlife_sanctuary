af2b4d82d4e28c2c06d4036b011c9d0cc68ee928
HemanthNRao <hemanthraokn355@gmail.com>
2018-09-14 16:25:46 +0530

    temp file

---
+++ sample.py
@@ -0,0 +1,11 @@
+a=int(input("enter the first number"));
+b=int(input("enter the second number"));
+print("addtion is:",a+b);
+print("subtraction is:",a-b);
+print("multiplication is:",a*b);
+print("modulo is",a%b);
+print("devision is:",a/b);
+print("modualo is:",a%b);
+
+>>>>>>> 4a5a7d98837214a8115a6fa1e84e51ffd8758611
+
